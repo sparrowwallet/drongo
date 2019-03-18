4f2c055d1d1abf9fcdebb737eb710d9f2b258551
Craig Raw <craig@quirk.biz>
2019-03-18 17:21:11 +0200

    check wallets #2

--- src/main/java/com/craigraw/drongo/TransactionTask.java
+++ src/main/java/com/craigraw/drongo/TransactionTask.java
@@ -1,7 +1,6 @@
 package com.craigraw.drongo;
 
 import com.craigraw.drongo.address.Address;
-import com.craigraw.drongo.crypto.ChildNumber;
 import com.craigraw.drongo.protocol.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
