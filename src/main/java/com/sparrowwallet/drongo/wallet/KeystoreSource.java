c675e395dbbf386d2371ac3819b2a4539bf25e95
Craig Raw <craigraw@gmail.com>
2020-05-11 11:37:25 +0200

    keystore source display

--- src/main/java/com/sparrowwallet/drongo/wallet/KeystoreSource.java
+++ src/main/java/com/sparrowwallet/drongo/wallet/KeystoreSource.java
@@ -1,5 +1,15 @@
 package com.sparrowwallet.drongo.wallet;
 
 public enum KeystoreSource {
-    HW_USB, HW_AIRGAPPED, SW_SEED, SW_WATCH;
+    HW_USB("Connected Hardware Wallet"), HW_AIRGAPPED("Airgapped Hardware Wallet"), SW_SEED("Software Wallet"), SW_WATCH("Watch Only Wallet");
+
+    private String displayName;
+
+    KeystoreSource(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
