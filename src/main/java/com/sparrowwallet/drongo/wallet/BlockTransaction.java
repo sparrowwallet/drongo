75701c725d00a11f7a3931190d029378a04be8f6
Craig Raw <craigraw@gmail.com>
2020-06-11 16:59:24 +0200

    keep block hash

--- src/main/java/com/sparrowwallet/drongo/wallet/BlockTransaction.java
+++ src/main/java/com/sparrowwallet/drongo/wallet/BlockTransaction.java
@@ -7,14 +7,24 @@ import java.util.Date;
 
 public class BlockTransaction extends BlockTransactionHash implements Comparable<BlockTransaction> {
     private final Transaction transaction;
+    private final Sha256Hash blockHash;
 
     public BlockTransaction(Sha256Hash hash, int height, Date date, Long fee, Transaction transaction) {
+        this(hash, height, date, fee, transaction, null);
+    }
+
+    public BlockTransaction(Sha256Hash hash, int height, Date date, Long fee, Transaction transaction, Sha256Hash blockHash) {
         super(hash, height, date, fee);
         this.transaction = transaction;
+        this.blockHash = blockHash;
     }
 
     public Transaction getTransaction() {
         return transaction;
+    }
+
+    public Sha256Hash getBlockHash() {
+        return blockHash;
     }
 
     @Override
