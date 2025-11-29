7244c3520bf30a928044fc36b53e71f4552ac839
Johannes Zweng <johannes@zweng.at>
2025-11-29 14:49:54 +0100

    improve calculation of Taproot tweaked output key

--- src/main/java/com/sparrowwallet/drongo/crypto/ECKey.java
+++ src/main/java/com/sparrowwallet/drongo/crypto/ECKey.java
@@ -488,53 +488,36 @@ public class ECKey {
     }
 
     public ECKey getTweakedOutputKey() {
-        TaprootPubKey taprootPubKey = liftX(getPubKeyXCoord());
-        ECPoint internalKey = taprootPubKey.ecPoint;
-        byte[] taggedHash = Utils.taggedHash("TapTweak", internalKey.getXCoord().getEncoded());
-        ECKey tweakValue = ECKey.fromPrivate(taggedHash);
-        ECPoint outputKey = internalKey.add(tweakValue.getPubKeyPoint());
+        BigInteger curveOrderN = CURVE_PARAMS.getCurve().getOrder();
+        ECPoint pubKey = pub.get();  // our internal key’s *actual* public key (before normalization to even y)
 
-        if(hasPrivKey()) {
-            BigInteger taprootPriv = priv;
-            BigInteger tweakedPrivKey = taprootPriv.add(tweakValue.getPrivKey()).mod(CURVE_PARAMS.getCurve().getOrder());
-            //TODO: Improve on this hack. How do we know whether to negate the private key before tweaking it?
-            if(!ECKey.fromPrivate(tweakedPrivKey).getPubKeyPoint().equals(outputKey)) {
-                taprootPriv = CURVE_PARAMS.getCurve().getOrder().subtract(priv);
-                tweakedPrivKey = taprootPriv.add(tweakValue.getPrivKey()).mod(CURVE_PARAMS.getCurve().getOrder());
+        boolean yWasOdd = pubKey.getAffineYCoord().toBigInteger().testBit(0);
+        if (yWasOdd) {
+            pubKey = pubKey.negate();
+        }
+
+        // Compute tweak value
+        byte[] tweakHash = Utils.taggedHash("TapTweak", pubKey.getXCoord().getEncoded());
+
+        // tweak: pubkey
+        ECPoint outputPub = pubKey.add(ECKey.fromPrivate(tweakHash).getPubKeyPoint());
+
+
+        if (hasPrivKey()) {
+            BigInteger privKey = getPrivKey();
+            if (yWasOdd) {
+                // if we negated the pubkey, negate the privkey too
+                privKey = curveOrderN.subtract(privKey);
             }
-
-            return new ECKey(tweakedPrivKey, outputKey, true);
+            // tweak private key
+            BigInteger outputPriv = privKey.add(new BigInteger(1, tweakHash)).mod(curveOrderN);
+            return new ECKey(outputPriv, outputPub, true);
         }
 
-        return ECKey.fromPublicOnly(outputKey, true);
+        // public-key-only:
+        return ECKey.fromPublicOnly(outputPub, true);
     }
 
-    private static TaprootPubKey liftX(byte[] bytes) {
-        SecP256K1Curve secP256K1Curve = (SecP256K1Curve)CURVE_PARAMS.getCurve();
-        BigInteger x = new BigInteger(1, bytes);
-        BigInteger p = secP256K1Curve.getQ();
-        if(x.compareTo(p) > -1) {
-            throw new IllegalArgumentException("Provided bytes must be less than secp256k1 field size");
-        }
-
-        BigInteger y_sq = x.modPow(BigInteger.valueOf(3), p).add(BigInteger.valueOf(7)).mod(p);
-        BigInteger y = y_sq.modPow(p.add(BigInteger.valueOf(1)).divide(BigInteger.valueOf(4)), p);
-        if(!y.modPow(BigInteger.valueOf(2), p).equals(y_sq)) {
-            throw new IllegalStateException("Calculated invalid y_sq when solving for y co-ordinate");
-        }
-
-        return y.and(BigInteger.ONE).equals(BigInteger.ZERO) ? new TaprootPubKey(secP256K1Curve.createPoint(x, y), false) : new TaprootPubKey(secP256K1Curve.createPoint(x, p.subtract(y)), true);
-    }
-
-    private static class TaprootPubKey {
-        public final ECPoint ecPoint;
-        public final boolean negated;
-
-        public TaprootPubKey(ECPoint ecPoint, boolean negated) {
-            this.ecPoint = ecPoint;
-            this.negated = negated;
-        }
-    }
 
     /**
      * Returns true if the given pubkey is canonical, i.e. the correct length taking into account compression.
